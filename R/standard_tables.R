407fed0052a167cba36e5dbe19353c28a7a38bc1
Jyoti Dubey <jyotidubey505@gmail.com>
2023-09-14 17:56:30 -0400

    Adding visualization table for Expected visits and form completions for WBS

--- R/standard_tables.R
+++ R/standard_tables.R
@@ -56,3 +56,86 @@ enrollment_status_by_site <- function(analytic){
   return(table)
 }
 
+
+
+
+
+#' Expected, completed, missing, out of window visits by each form
+#'
+#' @description This function visualizes the expected visits for each timepoint for MRR, CFU, PFU, BPI, AOS, KOOS forms
+#'
+#' @param analytic This is the analytic data set that must include mrr_status_6wk, mrr_status_3mo, mrr_status_6mo, 
+#' mrr_status_12mo, cfu_status_6wk, cfu_status_3mo, cfu_status_6mo, cfu_status_12mo, pfu_status_6wk, pfu_status_3mo, 
+#' pfu_status_6mo, pfu_status_12mo, bpi_status_6wk, bpi_status_3mo, bpi_status_6mo,bpi_status_12mo, aos_status_6wk, 
+#' aos_status_3mo, aos_status_6mo, aos_status_12mo, koos_status_6wk, koos_status_3mo, koos_status_6mo, koos_status_12mo, 
+#' sixweek_visit_expected, threemonth_visit_expected, sixmonth_visit_expected, twelvemonth_visit_expected, injury_type
+#'
+#' @return nothing
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' visit_status_for_followup_by_form()
+#' }
+visit_status_for_followup_by_form <- function(analytic){
+  
+  df <- analytic %>% 
+    select(mrr_status_6wk, mrr_status_3mo, mrr_status_6mo, mrr_status_12mo, cfu_status_6wk, cfu_status_3mo, cfu_status_6mo, 
+           cfu_status_12mo, pfu_status_6wk, pfu_status_3mo, pfu_status_6mo, pfu_status_12mo, bpi_status_6wk, bpi_status_3mo, bpi_status_6mo, 
+           bpi_status_12mo, aos_status_6wk, aos_status_3mo, aos_status_6mo, aos_status_12mo, koos_status_6wk, koos_status_3mo, koos_status_6mo, 
+           koos_status_12mo)
+  
+  df_expected <- analytic %>% 
+    select(sixweek_visit_expected, threemonth_visit_expected, sixmonth_visit_expected, twelvemonth_visit_expected) %>% 
+    summarise("12 Months"= sum(twelvemonth_visit_expected, na.rm = TRUE), "3 Months"= sum(threemonth_visit_expected, na.rm = TRUE),
+              "6 Months"= sum(sixmonth_visit_expected, na.rm = TRUE),  "6 Weeks"= sum(sixweek_visit_expected, na.rm = TRUE)) %>% 
+    mutate(Form = "Enrolled") %>% 
+    mutate(Status = "Expected") 
+  
+  df_injury_expected <- analytic %>% 
+    select(sixweek_visit_expected, threemonth_visit_expected, sixmonth_visit_expected, twelvemonth_visit_expected, injury_type) %>%
+    group_by(injury_type) %>% 
+    summarise("12 Months"= sum(twelvemonth_visit_expected, na.rm = TRUE), "3 Months"= sum(threemonth_visit_expected, na.rm = TRUE),
+              "6 Months"= sum(sixmonth_visit_expected, na.rm = TRUE),  "6 Weeks"= sum(sixweek_visit_expected, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    mutate(Form = str_to_title(injury_type))%>% 
+    select(-injury_type) %>% 
+    mutate(Status = "Expected") %>% 
+    filter(!is.na(Form))
+  
+  
+  output <- count_split_cols_long_and_wide(df, '_status_') %>%
+    rename(Form=prefix, Status=level)
+  
+  summed_completes <- summate_levels(output, 'Form', 'Status', c('Complete: Out of Window', 'Complete: In window')) %>%
+    mutate(Status='Complete')
+  output <- output %>%
+    filter(Status!='Complete: In window') %>%
+    bind_rows(summed_completes) %>% 
+    mutate(Status = ifelse(Status == 'Complete: Out of Window', "Out of Window", Status)) %>% 
+    mutate(Form = toupper(Form)) 
+  
+  bound_df <- bind_rows(output, df_expected, df_injury_expected) %>% 
+    ungroup()
+  
+  df_table_raw <- reorder_rows(bound_df, list('Form'=c("Enrolled", 'MRR', 'CFU', 'PFU', 'BPI',"Ankle", 'AOS',"Plateau", 'KOOS'), 
+                                              'Status'=c('Expected', 'Complete', 'Out of Window', 'Incomplete', 'Missing'))) %>%
+    mutate_if(is.numeric, replace_na, 0) 
+  
+  
+  index_vec <- c(" " = 1, "MRR"=4,"CFU"=4, "PFU"=4, "BPI"=4,  "AOS"= 5, "KOOS"=5)
+  
+  df_for_table <- df_table_raw %>% 
+    ungroup() %>% 
+    select(-Form) %>% 
+    rename("Form Completion"=Status)
+  
+  table_raw<- kable(df_for_table, align='l', padding='2l') %>%
+    pack_rows(index = index_vec) %>% 
+    add_indent(c(3,7,11,15,20,25)) %>% 
+    kable_styling("striped", full_width = F, position='left') %>% 
+    row_spec(c(0,1,18,23), extra_css = "border-bottom: 1px solid;")
+  
+  return(table_raw)
+}
+
