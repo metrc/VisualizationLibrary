00d71a9afe0c42534128a8bdc68d87ad8a2738f4
Elias Weston-Farber <elias@Eliass-MacBook-Pro-2.local>
2023-10-14 18:03:43 -0400

    bug fix

--- R/standard_tables.R
+++ R/standard_tables.R
@@ -359,7 +359,8 @@ discontinuation_sae_deviation_by_type <- function(analytic){
     filter(enrolled == TRUE) %>% 
     count(enrolled_discontinuation_reason) %>%
     rename(type=enrolled_discontinuation_reason) %>% 
-    filter(!is.na(type))
+    filter(!is.na(type)) %>% 
+    mutate(type = as.character(type))
   
   discontinuation_df_tot <- tibble(type="Discontinuations", n=sum(discontinuation_df$n))
   
@@ -369,7 +370,8 @@ discontinuation_sae_deviation_by_type <- function(analytic){
     mutate(sae_reported = "SAE") %>% 
     count(sae_reported) %>%
     rename(type=sae_reported) %>% 
-    filter(!is.na(type))
+    filter(!is.na(type)) %>% 
+    mutate(type = as.character(type))
   
   
   deviation_sc_df <- analytic %>% 
@@ -377,7 +379,8 @@ discontinuation_sae_deviation_by_type <- function(analytic){
     filter(enrolled == TRUE) %>% 
     count(deviation_screen_consent) %>%
     rename(type=deviation_screen_consent) %>% 
-    filter(!is.na(type))
+    filter(!is.na(type)) %>% 
+    mutate(type = as.character(type))
   
   
   deviation_p_df <- analytic %>% 
@@ -385,7 +388,8 @@ discontinuation_sae_deviation_by_type <- function(analytic){
     filter(enrolled == TRUE) %>% 
     count(deviation_procedural) %>%
     rename(type=deviation_procedural) %>% 
-    filter(!is.na(type))
+    filter(!is.na(type)) %>% 
+    mutate(type = as.character(type))
   
   
   deviation_a_df <- analytic %>% 
@@ -394,7 +398,8 @@ discontinuation_sae_deviation_by_type <- function(analytic){
     count(deviation_administrative) %>%
     rename(type=deviation_administrative) %>% 
     filter(!is.na(type)) %>% 
-    mutate(type = str_replace(type,"Other: .+","Other"))
+    mutate(type = str_replace(type,"Other: .+","Other")) %>% 
+    mutate(type = as.character(type))
   
   deviation_sc_tot <- tibble(type="Screen and Consent",n=sum(deviation_sc_df$n))
   deviation_p_tot <- tibble(type="Procedural",n=sum(deviation_p_df$n))
