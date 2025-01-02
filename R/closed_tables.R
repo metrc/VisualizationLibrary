ab74dc910d7452b730ec7c5d32b34d6788c1526f
Elias Weston-Farber <eweston4@jhu.edu>
2025-01-02 16:35:03 -0500

    bug fix

--- R/closed_tables.R
+++ R/closed_tables.R
@@ -421,7 +421,7 @@ closed_not_complete_sae_deviation_by_type <- function(analytic){
     indents_vec <- c(indents_vec, 1 + n_act + 1 + n_disc + 1 + 1 + 1 + n_dsc + 1 + n_dp + 1 + seq(n_da))
   }
   
-  indents_vec <- indents_vec[indents_vec <= nrow(df_final)]
+  indents_vec <- indents_vec[indents_vec <= nrow(df_table)]
   
   first_indents_vec <- c(ifelse(n_act==0, vector(mode="integer"), seq(n_act) + 1),
                          ifelse(n_disc==0, vector(mode="integer"),seq(n_disc) + 1 + n_act + 1), seq(1+n_dsc+1+n_dp+1+n_da) + 1 + n_act + 1 + n_disc + 1 + 1)
