7af01b0b3e66b9742a61a703bca22198d4216865
maytag42 <johnteague2000@gmail.com>
2025-11-18 14:56:06 -0500

    enrolled update for hardware_duration_statistics

--- R/standard_tables.R
+++ R/standard_tables.R
@@ -5423,12 +5423,12 @@ hardware_duration_statistics_by_site <- function(analytic, delta = FALSE){
   if (delta) {
     df1 <- analytic %>%  
       select(hardware_application_date, hardware_application_datetime, hardware_removal_date, hardware_removal_datetime, 
-             hardware_removal_date_missing, hardware_duration, hardware_delta, facilitycode) 
+             hardware_removal_date_missing, hardware_duration, hardware_delta, facilitycode, enrolled) 
     df1 <- df1 %>% mutate(start = hardware_application_date, end = hardware_removal_date, len = hardware_delta)
   } else {
     df1 <- analytic %>%  
       select(hardware_application_date, hardware_application_datetime, hardware_removal_date, hardware_removal_datetime, 
-             hardware_removal_date_missing, hardware_duration, facilitycode) 
+             hardware_removal_date_missing, hardware_duration, facilitycode, enrolled) 
     df1 <- df1 %>% mutate(start = hardware_application_datetime, end = hardware_removal_datetime, len = hardware_duration)
   }
   
